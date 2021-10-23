890462aee348c4903938c5dce2f652784f1b6894
fischerdavis <fischerdavis94@gmail.com>
2021-10-23 10:11:14 -0500

    Refactored skills component.

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -1,25 +1,27 @@
 import React from "react";
 import "./skills.scss";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { skillsList } from "./skillsList";
 
 const Skills = () => {
   return (
     <div className="skills-container" id="skills">
       <h1>Tech</h1>
       <div className="skills-items">
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" alt="C#"/>
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="HTML" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt="SASS" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" alt="C" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" alt="C++" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="ReactJs" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="Javascript" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg" alt="ElectronJs" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain-wordmark.svg" alt="MSSQL" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg" alt="Azure" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt="Bootstrap" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" alt="Docker" />
-        <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="Github" />
+
+        {skillsList.map((item, index) => (
+          <OverlayTrigger
+            key={index}
+            placement="top"
+            overlay={
+              <Tooltip id="tooltip-top">
+                Tooltip on <strong>{item.name}</strong>.
+              </Tooltip>
+            }
+          >
+            <img className="icon" src={item.link} alt={item.name} />
+          </OverlayTrigger>
+        ))}
       </div>
     </div>
   );
---
+++ src/pages/skills/skillsList.js
@@ -0,0 +1,58 @@
+export const skillsList = [
+  {
+    name: "C#",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+  },
+  {
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+  },
+  {
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
+  },
+  {
+    name: "SASS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+  },
+  {
+    name: "C",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
+  },
+  {
+    name: "C++",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+  },
+  {
+    name: "React",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+  },
+  {
+    name: "Javascript",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+  },
+  {
+    name: "ElectronJs",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg"
+  },
+  {
+    name: "MSSQL (Sql Server)",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain-wordmark.svg"
+  },
+  {
+    name: "Azure",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg"
+  },
+  {
+    name: "Bootstrap",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+  },
+  {
+    name: "Docker",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+  },
+  {
+    name: "Github",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+  }
+]
