98c2ba69ebb1db7bc3da4472e88bcf3af0763abf
fischerdavis <fischerdavis94@gmail.com>
2021-10-23 10:14:42 -0500

    Updated order of skills

--- src/pages/skills/skillsList.js
+++ src/pages/skills/skillsList.js
@@ -4,32 +4,36 @@ export const skillsList = [
     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
   },
   {
-    name: "CSS",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-  },
-  {
-    name: "HTML",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
-  },
-  {
-    name: "Sass",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+    name: "C++",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
   },
   {
     name: "C",
     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
   },
   {
-    name: "C++",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
   },
   {
-    name: "React",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+  },
+  {
+    name: "Sass",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+  },
+  {
+    name: "Bootstrap",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
   },
   {
     name: "Javascript",
     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+  },
+  {
+    name: "React",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
   },
   {
     name: "ElectronJs",
@@ -42,10 +46,6 @@ export const skillsList = [
   {
     name: "Azure",
     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg"
-  },
-  {
-    name: "Bootstrap",
-    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
   },
   {
     name: "Docker",
